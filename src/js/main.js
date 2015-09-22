8d5a451e0c405f1f38ca6770d785d45e2990aea3
smithdamen <smith.damen@gmail.com>
2015-09-22 02:15:33 -0400

    modified styles

--- src/js/main.js
+++ src/js/main.js
@@ -13,30 +13,31 @@ $(document).ready(function(){
   
   $('.mix-container').mixItUp();
   
-  //Waypoints
+  
   function onScrollInit( items, trigger ) {
     items.each( function() {
       var osElement = $(this),
         osAnimationClass = osElement.attr('data-os-animation'),
         osAnimationDelay = osElement.attr('data-os-animation-delay');
- 
-      osElement.css({
-        '-webkit-animation-delay':  osAnimationDelay,
-        '-moz-animation-delay':     osAnimationDelay,
-        'animation-delay':          osAnimationDelay
+      
+        osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+        });
+
+        var osTrigger = ( trigger ) ? trigger : osElement;
+        
+        osTrigger.waypoint(function() {
+          osElement.addClass('animated').addClass(osAnimationClass);
+          },{
+            triggerOnce: true,
+            offset: '90%'
       });
- 
-      var osTrigger = ( trigger ) ? trigger : osElement;
- 
-      osTrigger.waypoint(function() {
-        osElement.addClass('animated').addClass(osAnimationClass);
-      },{
-        triggerOnce: true,
-        offset: '90%'
-      });
-    });
-  }
-  
-  onScrollInit( $('.os-animation') );
-  
+  });
+}
+
+onScrollInit( $('.os-animation') );
+
 });
+
